ee2e98cb363e978cbd101adc136b5d0f2ae86131
DongKonLee <dle0129@gmail.com>
2021-07-27 21:08:08 +0900

    Object Mapper

--- hello/src/main/java/com/example/hello/controller/ApiController.java
+++ hello/src/main/java/com/example/hello/controller/ApiController.java
@@ -1,8 +1,9 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // 해당 Class 는 REST API 처리하는 Controller
 @RequestMapping("/api") // RequestMapping URI 를 지정해주는 Annotation
@@ -11,5 +12,21 @@ public class ApiController {
     @GetMapping("/hello") // http://localhost:9090/api/hello
     public String hello() {
         return "hello spring boot!";
+    }
+
+    @GetMapping("/text")
+    public String text(@RequestParam String account) {
+        return account;
+    }
+
+    // req -> object mapper -> object -> method -> object -> object mapper -> json -> res
+    @PostMapping("/json")
+    public User json(@RequestBody User user) {
+        return user; // 200 ok
+    }
+
+    @PutMapping("/put02")
+    public ResponseEntity<User> put(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
---
+++ hello/src/main/java/com/example/hello/controller/PageController.java
@@ -0,0 +1,26 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/main")
+    public String main() {
+        return "main.html";
+    }
+
+    @ResponseBody
+    @GetMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setName("steve");
+        user.setAddress("home");
+
+        return user;
+    }
+}
