4855111ed76b6d49181bee5a3b97686a55dfaf36
DongKonLee <dle0129@gmail.com>
2021-08-06 20:35:03 +0900

    Rest Template

---
+++ server_to_server/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,96 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserRequest;
+import com.example.client.dto.UserResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    // http://localhost:9090/api/server/hello
+    // response
+    public UserResponse hello() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .queryParam("name", "steve")
+                .queryParam("age", 10)
+                .encode()
+                .build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+
+    public UserResponse post() {
+        // http://localhost:9090/api/server/user/{userId}/name/{userName}
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setAge(10);
+        req.setName("steve");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        return response.getBody();
+    }
+
+    public UserResponse exchange() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setAge(10);
+        req.setName("steve");
+
+        RequestEntity<UserRequest> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization", "abcd")
+                .header("custom-header", "fffff")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.exchange(requestEntity, UserResponse.class);
+
+        return response.getBody();
+    }
+}
