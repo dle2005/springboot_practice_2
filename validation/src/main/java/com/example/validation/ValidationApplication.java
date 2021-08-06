b5de71d92e3f91021bb0669c22b4f80970a5e24a
DongKonLee <dle0129@gmail.com>
2021-08-06 17:50:49 +0900

    Async Process

--- validation/src/main/java/com/example/validation/ValidationApplication.java
+++ validation/src/main/java/com/example/validation/ValidationApplication.java
@@ -3,9 +3,11 @@ package com.example.validation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableAsync
 public class ValidationApplication {
 
     public static void main(String[] args) {
---
+++ validation/src/main/java/com/example/validation/controller/AsyncController.java
@@ -0,0 +1,26 @@
+package com.example.validation.controller;
+
+import com.example.validation.service.AsyncService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@RestController
+@RequestMapping("/async")
+public class AsyncController {
+
+    @Autowired
+    private AsyncService asyncService;
+
+    @GetMapping("/hello")
+    public CompletableFuture hello() {
+        log.info("completable future init");
+
+        return asyncService.run();
+    }
+}
