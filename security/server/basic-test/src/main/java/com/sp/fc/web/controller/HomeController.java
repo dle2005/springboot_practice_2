ecc0a03b3a1bf83b5682046a86a13cde7c0a6b41
DongKonLee <dle0129@gmail.com>
2022-04-05 17:26:49 +0900

    Security Basic
    - 접근 권한 설정
    - User 생성

---
+++ security/server/basic-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.sp.fc.web.controller;
+
+import com.sp.fc.web.domain.SecurityMessage;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @RequestMapping("/auth")
+    public Authentication auth() {
+        return SecurityContextHolder.getContext()
+                .getAuthentication();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+    @RequestMapping("/user")
+    public SecurityMessage user() {
+        return SecurityMessage.builder()
+                .auth(SecurityContextHolder.getContext().getAuthentication())
+                .message("user info")
+                .build();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    @RequestMapping("/admin")
+    public SecurityMessage admin() {
+        return SecurityMessage.builder()
+                .auth(SecurityContextHolder.getContext().getAuthentication())
+                .message("admin info")
+                .build();
+    }
+}
